df6738f6a93754299b40a9ec92b7d073de818466
Koki Takahashi <hakatasiloving@gmail.com>
2017-08-30 01:52:14 +0900

    Add eslintrc and perform eslint --fix

--- tests/lib/rules/array-plural.js
+++ tests/lib/rules/array-plural.js
@@ -2,36 +2,37 @@
  * @fileoverview Force usage of plural noun to name array variables
  * @author Koki Takahashi
  */
-"use strict";
 
-//------------------------------------------------------------------------------
+'use strict';
+
+// ------------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/array-plural"),
+let rule = require('../../../lib/rules/array-plural'),
 
-    RuleTester = require("eslint").RuleTester;
+	RuleTester = require('eslint').RuleTester;
 
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Tests
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("array-plural", rule, {
+const ruleTester = new RuleTester();
+ruleTester.run('array-plural', rule, {
 
-    valid: [
+	valid: [
 
-        // give me some code that won't trigger a warning
-    ],
+		// give me some code that won't trigger a warning
+	],
 
-    invalid: [
-        {
-            code: "const number = [0, 1, 2];",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
+	invalid: [
+		{
+			code: 'const number = [0, 1, 2];',
+			errors: [{
+				message: 'Fill me in.',
+				type: 'Me too',
+			}],
+		},
+	],
 });
